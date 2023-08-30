0c6e93d6d6dfebc3a9a21f40e364285d59a84934
Ezra Jeonadab G. Del Rosario <128983643+oji-py@users.noreply.github.com>
2023-08-30 15:06:15 +0800

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -109,7 +109,7 @@ cols = 3
 rows = int(1 + pics_num//cols)
 grid = [st.columns(cols) for row in range(rows)]
 cols_list = [column for row in grid for column in row]
-for column, image in zip(cols_list, list(range(1,13))):
+for column, image in zip(cols_list, list(range(1,13))):  # pairs the photos and columns
     image = Image.open(f"{image}.JPG")
     image_ = ImageOps.exif_transpose(image)
     column.image(image_)
